abd916bc3bad6310c471b2473cd28361967df84c
metabadger <113887966+metabadger@users.noreply.github.com>
2022-11-07 11:38:40 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,16 @@
+var figure = $(".anim").hover( play, pause );
+
+function play() {  
+    $('anim', this).get(0).play(); 
+}
+function pause() {
+    $('anim', this).get(0).pause(); 
+}
+function toggleText() {
+    var text = document.getElementById("description");
+    if (text.style.display === "none") {
+      text.style.display = "grid";
+    } else {
+      text.style.display = "none";
+    }
+  }
